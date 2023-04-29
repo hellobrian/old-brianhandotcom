bc4d05877b29560d549c18ea3fbe4f92014cd846
hellobrian <brianhan@hey.com>
2023-04-28 23:37:34 -0500

    feat: setup keyboardsCollection with pages using mdx

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { z, defineCollection } from "astro:content";
+
+const keyboardsCollection = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    size: z.enum([
+      "tkl",
+      "60-percent",
+      "65-percent",
+      "75-percent",
+      "alice",
+      "arisu",
+      "compact-1800",
+      "frl-1800",
+      "hhkb",
+    ]),
+    plate: z.enum(["brass", "fr4", "aluminum", "acrylic"]),
+  }),
+});
+
+export const collections = {
+  keyboards: keyboardsCollection,
+};
